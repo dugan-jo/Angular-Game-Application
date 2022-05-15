046f92f6c8c2a41bac8aebefb647d0b605e1cf01
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-05-15 16:49:26 -0600

    updated game card

---
+++ src/app/models.ts
@@ -0,0 +1,11 @@
+export interface Song {
+    background_image: string;
+    name: string;
+    released: string;
+    metacritic_url: string;
+    website: string;
+    description: string;
+    metacritic: number;
+    genres: Array<Genre>;
+    parent_platforms: Array<ParentPlatform>
+}
